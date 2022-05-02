98a5caa10cf55b18af88061fb4588ca58ca8032b
Andrew Rogers <104739481+aglrogers@users.noreply.github.com>
2022-05-02 19:54:46 +0100

    Updated code to include variable delay
    This script now incorporates a potentiometer connected to one of the analogue pins on the atTiny85 chip, allowing the user to vary the speed at which the lights flash.

--- atTiny85-PCF8574-light-chaser/atTiny85-PCF8574-light-chaser.ino
+++ atTiny85-PCF8574-light-chaser/atTiny85-PCF8574-light-chaser.ino
@@ -1,6 +1,7 @@
 /*
  * Test code for I2C configurations with the PCF8574 IO expander IC and the atTiny85 microcontroller.
- * Turns on all eight LEDs connected to the PCF8574 one after another, and then turns them all off again. Repeats forever.
+ * Turns on all eight LEDs connected to the PCF8574 one after another, and then turns them all off again. 
+ * A potentiometer is also used to control the speed of the pattern. Repeats forever.
  */
 
 #include <A85_PCF8574.h>  // Library to operate the PCF8574 with the atTiny85 microcontroller
@@ -8,11 +9,11 @@
 // Declare a PCF8574
 A85_PCF8574 myPCF;
 
-// Variable delay speed for the flashing effect
-const int flashDelay = 100;
-
 void setup()
 {
+  // Set analog pin A2 to INPUT for the potentiometer
+  pinMode(A2, INPUT);
+
   // Start the IO expander IC with I2C address 0x38
   myPCF.begin(0x38);
 }
@@ -25,7 +26,9 @@ void loop()
     for(int j = 0; j <= 7; j++)
     {
       myPCF.setBit(j, i);
-      delay(flashDelay);
+
+      // Delay code according to position of potentiometer
+      delay(map(analogRead(A2), 1, 1023, 2, 500));
     }
   }
 }
